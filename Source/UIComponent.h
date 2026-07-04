c529b59e9ab6554e8a469bd9a8266941056006bb
Dami <71833645+d-slam@users.noreply.github.com>
2026-07-04 11:31:37 +0200

    Add files via upload

--- Source/UIComponent.h
+++ Source/UIComponent.h
@@ -36,33 +36,48 @@ public:
 		g.drawRect(getLocalBounds(), 2);
 
 		g.setColour(juce::Colours::red);
-		g.drawEllipse(myPoint.getX(), myPoint.getY(), 5, 5, 5.0f);
+		//g.drawEllipse(myPoint.getX(), myPoint.getY(), 5, 5, 5.0f);
+
+
+		 // Alle aktiven Finger zeichnen
+		g.setColour(juce::Colours::red);
+
+		for (const auto& touch : touches)
+		{
+			auto pos = touch.second;
+
+			float radius = 10.0f;
+
+			g.fillEllipse(pos.x - radius,
+				pos.y - radius,
+				radius * 2.0f,
+				radius * 2.0f);
+		}
 
 	}
 
 
-	void mouseDown(const juce::MouseEvent& event) override
+	std::map<int, juce::Point<float>> touches;
+
+	void mouseDown(const juce::MouseEvent& e) override
 	{
-		auto pos = event.getPosition();
-		myPoint = event.getPosition();
-
-		DBG("x = " << pos.x);
-		DBG("y = " << pos.y);
-
+		touches[e.source.getIndex()] = e.position;
 		repaint();
-
 	}
 
-	void mouseDrag(const juce::MouseEvent& event) override
+	void mouseDrag(const juce::MouseEvent& e) override
 	{
-		auto pos = event.getPosition();
-		myPoint = event.getPosition();
-
-
-		DBG(pos.x << ", " << pos.y);
-
+		touches[e.source.getIndex()] = e.position;
 		repaint();
 	}
+
+	void mouseUp(const juce::MouseEvent& e) override
+	{
+		touches.erase(e.source.getIndex());
+		repaint();
+	}
+
+
 
 private:
 
